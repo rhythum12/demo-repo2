753941e8415bdebd448bbe323b0b6c623e240fd9
ashim <ashim.adhikari86@gmail.com>
2021-05-19 17:48:45 +0545

    java file is made
    changes were made

--- jv.java
+++ jv.java
@@ -1,5 +1,9 @@
 public class jv{
+    int a=4;
+    int b=9;
+    int c; 
     public static void main(String [] args){
-        System.out.println("Bhudey is a bitch");
+   
+        System.out.println("Bhudey is a bitch" + c);
     }
 }
